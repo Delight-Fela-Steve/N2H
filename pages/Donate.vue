6fec658a156d3698040742d77b866402666452b8
Delight-Fela-Steve <dfelastevegit@gmail.com>
2022-04-01 03:04:23 +0100

    added donate and buy pages

---
+++ pages/Donate.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <ul>
+            <li>Charity Organization 1</li>
+            <li>Charity Organization 2</li>
+            <li>Charity Organization 3</li>
+            <li>Charity Organization 4</li>
+            <li>Charity Organization 5</li>
+            <li>Charity Organization 6</li>
+            <li>Charity Organization 7</li>
+            <li>Charity Organization 8</li>
+            <li>Charity Organization 9</li>
+            <li>Charity Organization 10</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+ul{
+    list-style: none;
+    line-height: 3;
+    margin-top: 1rem;
+}
+li{
+    margin-bottom: 2rem;
+    border-left:1.5px solid black;
+    border-top: 1.5px solid black;
+    border-bottom: 1.5px solid black;
+    background: rgb(247, 143, 247);
+    padding-left: 0.5rem;
+}
+li:hover{
+    cursor:pointer;
+    background: rgb(253, 99, 253);
+}
+</style>
